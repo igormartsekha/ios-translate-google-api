20b4901d13b67c9e37053ebdc856d23750e40c73
Home <kvofreelance@gmail.com>
2016-06-03 15:05:07 +0300

    Remove no need code

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 var fs = require('fs');
+var path = require('path');
 var async = require('async');
-var googleTranslate = require('google-translate')("");
+var argv = require('minimist')(process.argv.slice(2));
 
-console.log("Start");
+var sourceFile = argv.sf;
+var sourceLanguage = argv.sl;
+var languages = argv.tl.split(",");
+var key = argv.k;
 
-var languages = ["he", "hi", "ms", "ko"];
+
+var googleTranslate = require('google-translate')(key);
+
+console.log("Start translation");
 
 function getKeysWordFromLocaliztionFile(filename, callback) {
     fs.readFile(filename, 'utf8', function (err, data) {
         if (err) return callback(err);
 
-        var globalRegExp = new RegExp(/\"(.*)?\"=\"(.*)?\";/g);
+        var globalRegExp = new RegExp(/\"(.*)?\".*=.*\"(.*)?\";/g);
         var allStrings = data.match(globalRegExp);
 
         var result = [];
-        var itemRexExp = new RegExp(/\"(.*)?\"=\"(.*)?\";/);
+        var itemRexExp = new RegExp(/\"(.*)?\".*=.*\"(.*)?\";/);
         for(var i=0; i < allStrings.length; i++) {
             var itemRaw  = allStrings[i].match(itemRexExp);
             result.push(itemRaw[1]);
@@ -24,56 +31,97 @@ function getKeysWordFromLocaliztionFile(filename, callback) {
     });
 }
 
+function translateLanguage(language, sourceLanguage, keyWords, callback) {
+    googleTranslate.translate(keyWords, sourceLanguage, language, function(err, translations) {
+        if(err)
+            return callback(err);
 
-
-function generateOutputLocalizableFile(item, callback) {
-    async.each(inputData, function (item, callback) {
-        var output = "";
-        var fileName = item.language + ".strings";
-        for(var j=0; j < item.result.length; j++) {
-            console.log(item.result[j]);
-            output += '"'+item.result[j].originalText+'"="'+item.result[j].translatedText+'";\n';
-        }
-
-        fs.writeFile(fileName, output, function(err) {
-            if(err) {
-                return callback(err);
-            }
-            console.log(output);
-            console.log("The file was saved!");
-            callback();
-        });
-    }, function (err) {
-        if(err) {
-            console.log('Smth. wrong: '+err);
-        } else {
-            console.log('Success');
-        }
+        var item = {
+            language: language,
+            result: translations
+        };
+        // =>  [{ translatedText: 'Hallo', originalText: 'Hello' }, ...]
+        callback(null, item);
     });
 }
 
-getKeysWordFromLocaliztionFile('Localizable.strings', function (err, keyToTranslate) {
-    console.log(err);
-//    console.log(result);
+function firstCharacterToUppercaseIfNeed(originalText, translatedText) {
+    if(originalText.length == 0)
+        return translatedText;
 
-    var translatedText = []
-    async.each(languages, function(language, callback) {
-        googleTranslate.translate(keyToTranslate, 'en', language, function(err, translations) {
-            translatedText.push({
-                language: language,
-                result: translations
-            });
-            // =>  [{ translatedText: 'Hallo', originalText: 'Hello' }, ...]
-            callback();
-        });
+    if(originalText[0] == originalText[0].toUpperCase()) {
+        translatedText = translatedText[0].toUpperCase() + translatedText.slice(1);
+    }
 
-    }, function(err){
-        if( err ) {
-            console.log('Error in trans: '+err);
-        } else {
-            console.log('Success');
-            console.log(translatedText);
-            generateOutputFile(translatedText);
-        }
+    return translatedText;
+}
+
+function ensureDirectoryExistence(filePath) {
+    var dirname = path.dirname(filePath);
+    if (directoryExists(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+
+function directoryExists(path) {
+    try {
+        return fs.statSync(path).isDirectory();
+    }
+    catch (err) {
+        return false;
+    }
+}
+
+function saveTranslatedData(item, callback) {
+    var output = "";
+    var filePath = 'output/'+item.language + "/"+sourceFile;
+    for(var j=0; j < item.result.length; j++) {
+        var originalText = item.result[j].originalText;
+        var translatedText = item.result[j].translatedText;
+
+        translatedText = firstCharacterToUppercaseIfNeed(originalText, translatedText);
+        output += '"'+originalText+'"="'+translatedText+'";\n';
+    }
+
+    ensureDirectoryExistence(filePath);
+    fs.writeFile(filePath, output, function(err) {
+        if(err)
+            return callback(err);
+
+        callback(null);
     });
+}
+
+async.waterfall([
+    function(callback) {
+        getKeysWordFromLocaliztionFile(sourceFile, callback);
+    },
+    function(keyWords, callback) {
+
+        async.each(languages, function (item, callbackInner) {
+            translateLanguage(item, sourceLanguage, keyWords, function (err, result) {
+                if(err) {
+                    console.log(err);
+                    return callbackInner();
+                }
+
+                saveTranslatedData(result, function (err) {
+                    if(err)
+                        console.log(err);
+
+                    console.log(item+": created");
+                    callbackInner();
+                })
+            });
+        }, function (err) {
+            if(err == null)
+                return callback(err);
+
+            callback(null);
+        });
+    }
+], function (err, result) {
+    console.log("Done");
 });
